4dc29d4fb20ee8968094f4801e7e4524ab280f18
Dragan Golubovic <dragolub2000@yahoo.com>
2022-09-25 11:37:44 +0200

    Verzija 1.0.0

---
+++ DataAdapterOleDBForm/DataAdapterOleDBForm/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace DataAdapterOleDBForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.dataGridView1);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 450);
+            this.splitContainer1.SplitterDistance = 66;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(273, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(252, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "OleDB DataSet primer";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(800, 380);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ DataAdapterOleDBForm/DataAdapterOleDBForm/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+         try
+           {
+               OleDbConnection conn = new OleDbConnection();
+               conn.ConnectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=c:\\testdatabase.xls;Extended Properties=\"Excel 8.0\"";
+               String strSQL = "Select * from Radnik2";
+               // Create an OleDbDataAdapter object  
+               OleDbDataAdapter adapter = new OleDbDataAdapter();
+               adapter.SelectCommand = new OleDbCommand(strSQL, conn);
+               // Create Data Set object  
+               DataSet ds = new DataSet("Radnici");
+               // Call DataAdapter's Fill method to fill data from the  
+               // DataAdapter to the DataSet  
+               adapter.Fill(ds);
+
+               // Bind dataset to a DataGrid control  
+               dataGridView1.DataSource = ds;//.DefaultViewManager;
+               dataGridView1.Show();
+
+               conn.Close();
+           }
+           catch (Exception ex)
+           {
+               //Console.WriteLine("Postoji problem: " + e.Message);
+               MessageBox.Show(ex.Message);
+           }
+        */
+
+namespace DataAdapterOleDBForm
+{
+   public partial class Form1 : Form
+   {
+       public Form1()
+       {
+           InitializeComponent();
+       }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                OleDbConnection conn = new OleDbConnection();
+                conn.ConnectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=c:\\testdatabase.xls;Extended Properties=\"Excel 8.0\"";
+                String strSQL = "Select * from Radnik2";
+                // Kreiranje  OleDbDataAdapter objekta  
+                OleDbDataAdapter adapter = new OleDbDataAdapter();
+                adapter.SelectCommand = new OleDbCommand(strSQL, conn);
+                // Kreiranje Data Set objekta!
+                DataSet ds = new DataSet();
+                adapter.Fill(ds,"Radnici");
+                dataGridView1.DataSource = ds;
+                dataGridView1.DataMember = "Radnici";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Postoji problem: " + ex.Message);
+            }
+        }
+    }
+}
